83f55f87ef65ab6a5fce47c4bfd14077ead77110
haidar-shiddiq <hdrshiddiq@gmail.com>
2021-11-28 15:52:12 +0700

    Login + Register form

---
+++ app/src/main/java/com/omellete/calmin/LoginActivity.java
@@ -0,0 +1,92 @@
+package com.omellete.calmin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.omellete.calmin.databinding.ActivityLoginBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.omellete.calmin.databinding.ActivityRegisterBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+    FirebaseAuth auth;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+        binding.textRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProgressDialog pd = new ProgressDialog(LoginActivity.this);
+                pd.setMessage("Mohon Tunggu Sebentar...");
+                pd.show();
+
+                String email = binding.emailLogin.getText().toString();
+                String password = binding.password.getText().toString();
+                if (TextUtils.isEmpty(email)||TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this, "Mohon isi semua field", Toast.LENGTH_SHORT).show();
+                }else{
+                    auth.signInWithEmailAndPassword(email,password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful ()){
+                                            reference = FirebaseDatabase.getInstance().getReference().child("Users")
+                                            .child(auth.getCurrentUser().getUid());
+
+                                            reference.addValueEventListener(new ValueEventListener() {
+                                                @Override
+                                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                    pd.dismiss ();
+                                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                    intent.addFlags (Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK) ;
+                                                    startActivity(intent);
+                                                    finish ();
+                                                }
+                                                @Override
+                                                public void onCancelled(@NonNull DatabaseError error) {
+                                                    pd.dismiss();
+                                                }
+                                            });
+
+                                    }else{
+                                        pd.dismiss();
+                                        Toast.makeText(LoginActivity.this, "Login gagal", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/omellete/calmin/RegisterActivity.java
@@ -0,0 +1,110 @@
+package com.omellete.calmin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.omellete.calmin.databinding.ActivityRegisterBinding;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    ActivityRegisterBinding binding;
+    FirebaseAuth auth;
+    DatabaseReference reference;
+    ProgressDialog pd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+        binding.textLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+
+        binding.registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pd = new ProgressDialog(RegisterActivity.this);
+                pd.setMessage("Mohon Tunggu Sebentar...");
+                pd.show();
+
+                String username = binding.username.getText().toString();
+                String email = binding.email.getText().toString();
+                String password = binding.password.getText().toString();
+                String confirmPw = binding.confirmPassword.getText().toString();
+
+                if (TextUtils.isEmpty(username)||TextUtils.isEmpty(email)||TextUtils.isEmpty(password)
+                ||TextUtils.isEmpty(confirmPw)){
+                    Toast.makeText(RegisterActivity.this, "Mohon isi semua field", Toast.LENGTH_SHORT).show();
+                    pd.dismiss();
+                }else if(password.length()<6){
+                    Toast.makeText(RegisterActivity.this, "Password minimal terdiri dari 6 karakter", Toast.LENGTH_SHORT).show();
+                    pd.dismiss();
+                }else if(!password.equals(confirmPw)){
+                    Toast.makeText(RegisterActivity.this, "Konfirmasi password dengan benar", Toast.LENGTH_SHORT).show();
+                    pd.dismiss();
+                }else{
+                    register(username,email,password);
+                }
+            }
+        });
+    }
+
+    private void register(String username, String email, String password){
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful ()) {
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userid = firebaseUser.getUid();
+                            reference = FirebaseDatabase.getInstance().getReference().child("Users").child(userid);
+
+                            HashMap<String, Object> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username", username.toLowerCase());
+                            hashMap.put("email", email);
+
+                            reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        pd.dismiss();
+                                        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                        intent.addFlags (Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent. FLAG_ACTIVITY_NEW_TASK) ;
+                                        startActivity(intent);
+                                    }
+                                }
+                            });
+                        }else{
+                            pd.dismiss();
+                            Toast.makeText(RegisterActivity.this, "Maaf, anda tidak bisa register dengan username ini", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+}
